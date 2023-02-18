65e7cfef05644bd9d4f4d23b691755517fee92b0
AlinaTaifurova <120791016+AlinaTaifurova@users.noreply.github.com>
2023-02-18 12:59:29 +0300

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.*/
+
+Console.WriteLine("Задайте прямоугольный двумерный массив:");
+int n = int.Parse(Console.ReadLine()!);
+int m = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[n, m];
+FillMatrixRandom(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+NumberRowMinSumElements(matrix);
+
+void NumberRowMinSumElements(int[,] matrix)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minRow += matrix[0, i];
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) sumRow += matrix[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка с наименьшей суммой элементов");
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrixRandom(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}    
